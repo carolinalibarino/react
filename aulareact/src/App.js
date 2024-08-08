75255adf8ff22072ce9022ccf2713fb30d6d2a33
carolinalibarino <carollibarino2@gmail.com>
2024-08-08 10:59:17 -0300

    react

--- aulareact/src/App.js
+++ aulareact/src/App.js
@@ -1,11 +1,18 @@
+import Container from "./components/Container"
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header/>
-      <h1>ETEC MCM</h1>
-      <p>Olá Mundo!</p>
+      <Banner/>
+      <Container>
+        <h1>ETEC MCM</h1>
+        <p>Olá Mundo!</p>
+      </Container>
+      <Footer/>
     </>
   );
 }
